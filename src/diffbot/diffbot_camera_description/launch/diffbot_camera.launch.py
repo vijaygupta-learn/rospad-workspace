1e1fc3226f3580a3cb12b67a81ad6ef94e1e1bca
Vijay <vijaygupta.learn@gmail.com>
2026-08-21 13:32:42 +0530

    Update src/diffbot/diffbot_camera_description/launch/diffbot_camera.launch.py

---
+++ src/diffbot/diffbot_camera_description/launch/diffbot_camera.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    urdf_path = os.path.join(
+        os.path.dirname(__file__), '..', 'urdf', 'diffbot_camera.urdf'
+    )
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+        ),
+    ])
